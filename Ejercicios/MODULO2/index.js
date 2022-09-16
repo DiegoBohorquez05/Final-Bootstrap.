4ce2b6aeb151f6ac7095c945a29f27fd332c0fec
Juan_Diego <judiboor@gmail.com>
2022-09-16 09:29:23 -0500

    js

--- Ejercicios/MODULO2/index.js
+++ Ejercicios/MODULO2/index.js
@@ -1,38 +1,48 @@
-const number = [20, 30, 40, 50]
-const person = { name: "pepe", lastname: "perez", age: 30, country: "Colombia" }
+// const number = [20, 30, 40, 50]
+// const person = { name: "pepe", lastname: "perez", age: 30, country: "Colombia" }
 
-for (let prop in person) {
-    console.log(prop)
+// for (let prop in person) {
+//     console.log(prop)
+// }
+
+// for (let numero of number) {
+//     console.log(numero)
+// }
+
+
+
+// //declaradas
+// function sumar() {
+//     let a = 1
+//     let b = 2
+//     let c = a + b
+//     console.log(c)
+// }
+// sumar()
+
+
+// //expresadas
+// const suma = function () {
+//     let a = 1
+//     let b = 2
+//     let c = a + b
+//     console.log(c)
+// }
+// suma()
+
+// //arrow funtion
+// const suma2=(a,b)=>{
+//     let suma=a+b
+//     console.log(suma)
+// }
+// suma2(1,2)
+
+
+//TABLA DE MULTIPLCIAR
+let tabla=+prompt(`Digite el numero de la tabla que quiere ver`)
+function multiplicar(tabla){
+    for(let i=1; i<10; i++){
+        document.write(`${tabla} x ${i} = ${tabla*i}`)
+    }
 }
-
-for (let numero of number) {
-    console.log(numero)
-}
-
-
-
-//declaradas
-function sumar() {
-    let a = 1
-    let b = 2
-    let c = a + b
-    console.log(c)
-}
-sumar()
-
-
-//expresadas
-const suma = function () {
-    let a = 1
-    let b = 2
-    let c = a + b
-    console.log(c)
-}
-suma()
-
-//arrow funtion
-const suma2=(a,b)=>{
-    let suma=a+b
-    console.log(suma)
-}
-suma2(1,2)
+multiplicar(tabla)
